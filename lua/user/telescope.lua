2ab07c6c9cced04454e1195f82b7365eded198f4
Luca Fedrizzi <9001053+fedriz@users.noreply.github.com>
2022-10-04 17:01:14 +0200

    Show dotfiles in telescope search

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -1,12 +1,18 @@
 local status_ok, telescope = pcall(require, "telescope")
 if not status_ok then
-    return
+        return
 end
 
 telescope.setup {
-    pickers = {
-        find_files = {
-            hidden = true
+        defaults = {
+                file_ignore_patterns = {
+                        "node_modules", ".git", "dist", "output", ".mypy_cache", "__pycache__", "*.pyc"
+                },
+        },
+        pickers = {
+                find_files = {
+                        hidden = true,
+                        no_ignore = true
+                }
         }
-    }
 }
