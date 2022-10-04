abfd4548d8763536240c7e3670259f057832d471
Luca Fedrizzi <9001053+fedriz@users.noreply.github.com>
2022-10-04 17:01:04 +0200

    Show dotfiles in tree, apart .git

--- lua/user/tree.lua
+++ lua/user/tree.lua
@@ -1,6 +1,11 @@
 local status_ok, tree = pcall(require, "nvim-tree")
 if not status_ok then
-    return
+        return
 end
 
-tree.setup()
+tree.setup({
+        filters = {
+                dotfiles = false,
+                custom = { "^.git$" }
+        },
+})
