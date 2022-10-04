ab87d5df29ff279b3c251852609dcb89d7527758
Luca Fedrizzi <9001053+fedriz@users.noreply.github.com>
2022-10-04 05:34:18 +0200

    Find hidden files

---
+++ lua/user/telescope.lua
@@ -0,0 +1,12 @@
+local status_ok, telescope = pcall(require, "telescope")
+if not status_ok then
+    return
+end
+
+telescope.setup {
+    pickers = {
+        find_files = {
+            hidden = true
+        }
+    }
+}
