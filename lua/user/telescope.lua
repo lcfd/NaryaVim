3935daabe5428650ef256171e7953f1c83602aad
Luca Fedrizzi <9001053+fedriz@users.noreply.github.com>
2022-10-15 17:58:07 +0200

    Format telescope file

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -1,19 +1,17 @@
 local status_ok, telescope = pcall(require, "telescope")
 if not status_ok then
-        return
+    return
 end
 
 telescope.setup {
-        defaults = {
-                file_ignore_patterns = {
-                        "node_modules", ".git", "dist", "output", ".mypy_cache", "__pycache__", "*.pyc", ".jekyll-cache",
-                        "venv"
-                },
-        },
-        pickers = {
-                find_files = {
-                        hidden = true,
-                        no_ignore = true
-                }
+    defaults = {
+        file_ignore_patterns = {"node_modules", ".git", "dist", "output", ".mypy_cache", "__pycache__", "*.pyc",
+                                ".jekyll-cache", "venv"}
+    },
+    pickers = {
+        find_files = {
+            hidden = true,
+            no_ignore = true
         }
+    }
 }
