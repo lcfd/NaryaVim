e6e41dce4eebf9624ff3c30850f382850a7a67ba
Luca Fedrizzi <9001053+fedriz@users.noreply.github.com>
2023-01-02 12:22:01 +0100

    Add neotree config, disable transparent, use better notify animation

--- lua/narya/misc.lua
+++ lua/narya/misc.lua
@@ -11,7 +11,8 @@ function M.setup()
         -- or leave it empty to use the default settings
         style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
         light_style = "day", -- The theme is used when the background is set to light
-        transparent = true, -- Enable this to disable setting the background color
+        -- transparent = true, -- Enable this to disable setting the background color
+        transparent = false, -- Enable this to disable setting the background color
         terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
         styles = {
             -- Style to be applied to different syntax groups
@@ -174,8 +175,8 @@ function M.setup()
     end
 
     notify.setup({
-        background_colour = "#00000090",
-        stages = "static"
+        -- background_colour = "#00000090",
+        stages = "fade_in_slide_out"
     })
 
     -- ######################
@@ -213,10 +214,207 @@ function M.setup()
             width = 90,
             options = {
                 number = true,
-                relativenumber = true,
+                relativenumber = true
+            }
+        }
+    }
+
+    -- ##########################
+    -- ######## neo-tree ########
+    -- ##########################
+
+    require("neo-tree").setup({
+        close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
+        popup_border_style = "rounded",
+        enable_git_status = true,
+        enable_diagnostics = true,
+        sort_case_insensitive = true, -- used when sorting files and directories in the tree
+        default_component_configs = {
+            container = {
+                enable_character_fade = true
+            },
+            indent = {
+                indent_size = 2,
+                padding = 1, -- extra padding on left hand side
+                -- indent guides
+                with_markers = true,
+                indent_marker = "│",
+                last_indent_marker = "└",
+                highlight = "NeoTreeIndentMarker",
+                -- expander config, needed for nesting files
+                with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
+                expander_collapsed = "",
+                expander_expanded = "",
+                expander_highlight = "NeoTreeExpander"
+            },
+            icon = {
+                folder_closed = "",
+                folder_open = "",
+                folder_empty = "ﰊ",
+                -- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
+                -- then these will never be used.
+                default = "*",
+                highlight = "NeoTreeFileIcon"
+            },
+            modified = {
+                symbol = "[+]",
+                highlight = "NeoTreeModified"
+            },
+            name = {
+                trailing_slash = false,
+                use_git_status_colors = true,
+                highlight = "NeoTreeFileName"
+            },
+            git_status = {
+                symbols = {
+                    -- Change type
+                    added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+                    modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
+                    deleted = "✖", -- this can only be used in the git_status source
+                    renamed = "", -- this can only be used in the git_status source
+                    -- Status type
+                    untracked = "",
+                    ignored = "",
+                    unstaged = "",
+                    staged = "",
+                    conflict = ""
+                }
             }
         },
-    }
+        window = {
+            position = "left",
+            width = 40,
+            mapping_options = {
+                noremap = true,
+                nowait = true
+            },
+            mappings = {
+                ["<space>"] = {
+                    "toggle_node",
+                    nowait = false -- disable `nowait` if you have existing combos starting with this char that you want to use 
+                },
+                ["<2-LeftMouse>"] = "open",
+                ["<cr>"] = "open",
+                ["<esc>"] = "revert_preview",
+                ["P"] = {
+                    "toggle_preview",
+                    config = {
+                        use_float = true
+                    }
+                },
+                ["S"] = "open_split",
+                ["s"] = "open_vsplit",
+                -- ["S"] = "split_with_window_picker",
+                -- ["s"] = "vsplit_with_window_picker",
+                ["t"] = "open_tabnew",
+                -- ["<cr>"] = "open_drop",
+                -- ["t"] = "open_tab_drop",
+                ["w"] = "open_with_window_picker",
+                -- ["P"] = "toggle_preview", -- enter preview mode, which shows the current node without focusing
+                ["C"] = "close_node",
+                ["z"] = "close_all_nodes",
+                -- ["Z"] = "expand_all_nodes",
+                ["a"] = {
+                    "add",
+                    -- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
+                    -- some commands may take optional config options, see `:h neo-tree-mappings` for details
+                    config = {
+                        show_path = "none" -- "none", "relative", "absolute"
+                    }
+                },
+                ["A"] = "add_directory", -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
+                ["d"] = "delete",
+                ["r"] = "rename",
+                ["y"] = "copy_to_clipboard",
+                ["x"] = "cut_to_clipboard",
+                ["p"] = "paste_from_clipboard",
+                ["c"] = "copy", -- takes text input for destination, also accepts the optional config.show_path option like "add":
+                -- ["c"] = {
+                --  "copy",
+                --  config = {
+                --    show_path = "none" -- "none", "relative", "absolute"
+                --  }
+                -- }
+                ["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
+                ["q"] = "close_window",
+                ["R"] = "refresh",
+                ["?"] = "show_help",
+                ["<"] = "prev_source",
+                [">"] = "next_source"
+            }
+        },
+        nesting_rules = {},
+        filesystem = {
+            filtered_items = {
+                visible = false, -- when true, they will just be displayed differently than normal items
+                hide_dotfiles = false,
+                hide_gitignored = false,
+                hide_hidden = true, -- only works on Windows for hidden files/directories
+                hide_by_name = {
+                    -- "node_modules"
+                },
+                hide_by_pattern = { -- uses glob style patterns
+                    -- "*.meta",
+                    -- "*/src/*/tsconfig.json",
+                },
+                always_show = { -- remains visible even if other settings would normally hide it
+                    ".gitignore",
+                    ".git"
+                },
+                never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+                    -- ".DS_Store",
+                    -- "thumbs.db"
+                },
+                never_show_by_pattern = { -- uses glob style patterns
+                    -- ".null-ls_*",
+                }
+            },
+            follow_current_file = false,
+            group_empty_dirs = false,
+            hijack_netrw_behavior = "disabled",
+            use_libuv_file_watcher = false, -- This will use the OS level file watchers to detect changes
+            -- instead of relying on nvim autocmd events.
+            window = {
+                mappings = {
+                    ["<bs>"] = "navigate_up",
+                    ["."] = "set_root",
+                    ["H"] = "toggle_hidden",
+                    ["/"] = "fuzzy_finder",
+                    ["D"] = "fuzzy_finder_directory",
+                    ["f"] = "filter_on_submit",
+                    ["<c-x>"] = "clear_filter",
+                    ["[g"] = "prev_git_modified",
+                    ["]g"] = "next_git_modified"
+                }
+            }
+        },
+        buffers = {
+            follow_current_file = true,
+            group_empty_dirs = false,
+            show_unloaded = true,
+            window = {
+                mappings = {
+                    ["bd"] = "buffer_delete",
+                    ["<bs>"] = "navigate_up",
+                    ["."] = "set_root"
+                }
+            }
+        },
+        git_status = {
+            window = {
+                position = "float",
+                mappings = {
+                    ["A"] = "git_add_all",
+                    ["gu"] = "git_unstage_file",
+                    ["ga"] = "git_add_file",
+                    ["gr"] = "git_revert_file",
+                    ["gc"] = "git_commit",
+                    ["gp"] = "git_push",
+                    ["gg"] = "git_commit_and_push"
+                }
+            }
+        }
+    }) -- End setup Neotree
 
 end
 
