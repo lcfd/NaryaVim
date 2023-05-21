d25ea9714559aac307607b378519c0ba3eb1dbf2
Luca Fedrizzi <9001053+fedriz@users.noreply.github.com>
2023-05-21 08:07:24 +0200

    Fix mypy config in null-ls
    No direct invoke, just use the default builtin

--- lua/narya/null-ls.lua
+++ lua/narya/null-ls.lua
@@ -9,7 +9,7 @@ function M.setup()
 
   local status_ok, mason_null_ls = pcall(require, "mason-null-ls")
   if not status_ok then
-    vim.notify("Problem with mason")
+    vim.notify("Problem with mason-null-ls")
     return
   end
 
@@ -22,13 +22,7 @@ function M.setup()
       null_ls.builtins.code_actions.eslint_d,
       null_ls.builtins.diagnostics.markdownlint,
       null_ls.builtins.diagnostics.proselint,
-      null_ls.builtins.diagnostics.mypy.with({
-        command = {
-          "python",
-          "-m",
-          "mypy",
-        },
-      }),
+      null_ls.builtins.diagnostics.mypy,
     },
     debug = true,
   })
