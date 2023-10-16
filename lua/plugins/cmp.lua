7f461b53c80e6722d1664e57768dcad5dd007432
Luca Fedrizzi <9001053+fedriz@users.noreply.github.com>
2023-10-16 13:20:20 +0200

    Simplify config

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,6 +3,7 @@
 
 return {
   "hrsh7th/nvim-cmp",
+  version = false,
   -- Load cmp on InsertEnter
   event = "InsertEnter",
   -- These dependencies will only be loaded when cmp loads.
@@ -22,8 +23,8 @@ return {
       vim.notify("No cmp", "error")
       return
     end
-    local status_ok, luasnip = pcall(require, "luasnip")
-    if not status_ok then
+    local lua_snip_status_ok, luasnip = pcall(require, "luasnip")
+    if not lua_snip_status_ok then
       vim.notify("Setup: No luasnip", "error")
       return
     end
@@ -79,13 +80,13 @@ return {
         }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         ["<C-p>"] = cmp.mapping.select_prev_item(),
         ["<C-n>"] = cmp.mapping.select_next_item(),
-        ["<C-j>"] = cmp.mapping(function(fallback)
+        ["<C-j>"] = cmp.mapping(function(_)
           luasnip.jump(1)
         end, {
           "i",
           "s",
         }),
-        ["<C-k>"] = cmp.mapping(function(fallback)
+        ["<C-k>"] = cmp.mapping(function(_)
           luasnip.jump(-1)
         end, {
           "i",
@@ -102,8 +103,10 @@ return {
           vim_item.kind = string.format("%s", icons[vim_item.kind])
           vim_item.menu = ({
             nvim_lsp = "[LSP]",
+            nvim_lsp_signature_help = "[LSPH]",
             luasnip = "[SN]",
             buffer = "[BU]",
+            treesitter = "[TST]",
             path = "[PA]",
           })[entry.source.name]
           return vim_item
@@ -116,12 +119,15 @@ return {
         {
           name = "nvim_lsp",
         },
+        { name = "path" },
         {
           name = "luasnip",
         },
         {
           name = "buffer",
-          -- keyword_length = 4,
+        },
+        {
+          name = "treesitter",
         },
       }),
 
---
+++ lua/toggle-checkbox.lua
@@ -0,0 +1,43 @@
+local checked_character = "x"
+
+local checked_checkbox = "%[" .. checked_character .. "%]"
+local unchecked_checkbox = "%[ %]"
+
+local line_contains_an_unchecked_checkbox = function(line)
+  return string.find(line, unchecked_checkbox)
+end
+
+local checkbox = {
+  check = function(line)
+    return line:gsub(unchecked_checkbox, checked_checkbox)
+  end,
+
+  uncheck = function(line)
+    return line:gsub(checked_checkbox, unchecked_checkbox)
+  end,
+}
+
+local M = {}
+
+M.toggle = function()
+  local bufnr = vim.api.nvim_buf_get_number(0)
+  local cursor = vim.api.nvim_win_get_cursor(0)
+  local start_line = cursor[1] - 1
+  local current_line = vim.api.nvim_buf_get_lines(bufnr, start_line, start_line + 1, false)[1] or ""
+
+  -- If the line contains a checked checkbox then uncheck it.
+  -- Otherwise, if it contains an unchecked checkbox, check it.
+  local new_line = ""
+  if line_contains_an_unchecked_checkbox(current_line) then
+    new_line = checkbox.check(current_line)
+  else
+    new_line = checkbox.uncheck(current_line)
+  end
+
+  vim.api.nvim_buf_set_lines(bufnr, start_line, start_line + 1, false, { new_line })
+  vim.api.nvim_win_set_cursor(0, cursor)
+end
+
+vim.api.nvim_create_user_command("ToggleCheckbox", M.toggle, {})
+
+return M
