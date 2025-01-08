14866759564ccfae990f2ee39e9e7d9299d585c9
Luca Fedrizzi <9001053+fedriz@users.noreply.github.com>
2025-01-08 11:33:49 +0100

    Re-add missing telescope commands

--- lua/plugins/utils/init.lua
+++ lua/plugins/utils/init.lua
@@ -2,13 +2,13 @@ local safe_import = require("utils.safe_import")
 
 return {
   -- Motion plugin
-  {
-    "ggandor/leap.nvim",
-    config = function()
-      vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)')
-      vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)')
-    end,
-  },
+  -- {
+  --   "ggandor/leap.nvim",
+  --   config = function()
+  --     vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)')
+  --     vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)')
+  --   end,
+  -- },
   {
     "numToStr/Comment.nvim",
     lazy = false,
